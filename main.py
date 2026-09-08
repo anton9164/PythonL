ad9545cffd66e5cca4be546c4e55936d3d31ab2c
Anton <sklar8012@gmail.com>
2026-09-08 20:00:17 +0200

    Basic programm in which i learn to use equality operators and Booleans Values

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+# This is a simple Python program that prints "Hello, World!"
+
+n = int(input("Enter a number: "))
+print(n >= 100)
+# This program takes an int input from user and checks if it is greater than or equal to 100, if it greater or equal than it will print True, else False
