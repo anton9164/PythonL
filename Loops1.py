cc90c71dbbaf6d8975be5cfc379ae0035a1dbd12
Anton <sklar8012@gmail.com>
2026-09-09 19:23:56 +0200

    counting programm with usage of 'for' loop

---
+++ Loops1.py
@@ -0,0 +1,6 @@
+#This program counts to 5, printing int i with a word Mississippi
+#After 5 times, loop ends and prints text
+import time
+for i in range(1,6):
+    print(i, "Mississippi")
+print("Ready or not, here I come!")
